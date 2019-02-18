99ecddf56b1b88e65f6993c120405c66541bd2a5
Reegeek <regis.fricker@gmail.com>
2019-02-18 17:19:03 +0100

    [ForEach] add ConvertTypedEnumerable in benchmark.

--- src/StructLinq.Benchmark/ForEach.cs
+++ src/StructLinq.Benchmark/ForEach.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using BenchmarkDotNet.Attributes;
 using StructLinq.ForEach;
+using StructLinq.IEnumerable;
 using StructLinq.Range;
 
 namespace StructLinq.Benchmark
@@ -11,13 +12,14 @@ namespace StructLinq.Benchmark
     {
         private readonly IEnumerable<int> sysRange;
         private readonly ITypedEnumerable<int, RangeEnumerator> structRange;
+        private readonly ITypedEnumerable<int, GenericEnumerator<int>> convertRange;
         private CountAction[] countActions = new CountAction[1];
         public int Count = 10000;
         public ForEach()
         {
             sysRange = Enumerable.Range(0, Count);
             structRange = StructEnumerable.Range(0, Count);
-            ref CountAction countAction = ref countActions[0];
+            convertRange = sysRange.ToTypedEnumerable();
         }
 
         [Benchmark(Baseline = true)]
@@ -45,7 +47,17 @@ namespace StructLinq.Benchmark
             countAction.Count = 0;
             structRange.ForEach(ref countAction);
             return countAction.Count;
-        } 
+        }
+
+        [Benchmark]
+        public int ConvertForEach()
+        {
+            ref CountAction countAction = ref countActions[0];
+            countAction.Count = 0;
+            convertRange.ForEach(ref countAction);
+            return countAction.Count;
+        }
+
 
         struct CountAction : IAction<int>
         {
