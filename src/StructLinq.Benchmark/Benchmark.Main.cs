ccffaa9b47eac15d42c51d7f84ac00dc4ba5470b
Reegeek <regis.fricker@gmail.com>
2019-02-18 16:16:01 +0100

    [Infra] Add Benchmark project.

---
+++ src/StructLinq.Benchmark/Benchmark.Main.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace StructLinq.Benchmark
+{
+    public class Program
+    {
+        public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+
+}
