1f02831169cade273c725a820b5c5fbcd0d37e07
Reegeek <regis.fricker@gmail.com>
2020-02-24 10:34:36 +0100

    [Infra] Update benchmark from 0.11.5 to 0.12.0

--- src/StructLinq.Benchmark/Benchmark.Main.cs
+++ src/StructLinq.Benchmark/Benchmark.Main.cs
@@ -1,9 +1,14 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System;
+using BenchmarkDotNet.Running;
 
 namespace StructLinq.Benchmark
 {
     public class Program
     {
-        public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            Console.ReadLine();
+        }
     }
 }
