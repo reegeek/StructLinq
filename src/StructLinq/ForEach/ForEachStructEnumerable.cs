d1d1ff7533806a2a75a456a0f63c0ebb48e418d6
Reegeek <regis.fricker@gmail.com>
2020-02-24 10:35:08 +0100

    [ForEach] Extract method to inline struct implementation.

--- src/StructLinq/ForEach/ForEachStructEnumerable.cs
+++ src/StructLinq/ForEach/ForEachStructEnumerable.cs
@@ -7,18 +7,27 @@ namespace StructLinq
 {
     public static partial class StructEnumerable
     {
+        private static void InternalForEach<T, TEnumerator, TAction>(TAction action, TEnumerator enumerator)
+            where TEnumerator : struct, IEnumerator<T>
+            where TAction : struct, IAction<T>
+        {
+            while (enumerator.MoveNext())
+            {
+                action.Do(enumerator.Current);
+            }
+        }
+
         public static void ForEach<T, TEnumerator, TAction>(this IStructEnumerable<T, TEnumerator> enumerable, ref TAction action) 
             where TEnumerator : struct, IEnumerator<T>
             where TAction : struct, IAction<T>
         {
             using (var enumerator = enumerable.GetStructEnumerator())
             {
-                while (enumerator.MoveNext())
-                {
-                    action.Do(enumerator.Current);
-                }
+                InternalForEach<T, TEnumerator, TAction>(action, enumerator);
             }
         }
+
+
         public static void ForEach<T, TEnumerator>(this IStructEnumerable<T, TEnumerator> enumerable, Action<T> action)
             where TEnumerator : struct, IEnumerator<T>
         {
