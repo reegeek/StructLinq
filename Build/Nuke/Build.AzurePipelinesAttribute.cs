78cd6d904d7e324558a8c5e23b3cace0e1e5b6d3
Reegeek <regis.fricker@gmail.com>
2020-04-19 17:29:00 +0200

    [Infra] add nuke project.

---
+++ Build/Nuke/Build.AzurePipelinesAttribute.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI.AzurePipelines;
+using Nuke.Common.CI.AzurePipelines.Configuration;
+using Nuke.Common.Execution;
+using Nuke.Common.Tooling;
+
+partial class Build
+{
+    public class AzurePipelinesAttribute : Nuke.Common.CI.AzurePipelines.AzurePipelinesAttribute
+    {
+        public AzurePipelinesAttribute(
+            string suffix,
+            AzurePipelinesImage image,
+            params AzurePipelinesImage[] images)
+            : base(suffix, image, images)
+        {
+        }
+
+        protected override AzurePipelinesStage GetStage(AzurePipelinesImage image, IReadOnlyCollection<ExecutableTarget> relevantTargets)
+        {
+            string[] targetToExcludes;
+            switch (image)
+            {
+                case AzurePipelinesImage.WindowsLatest:
+                case AzurePipelinesImage.Windows2019:
+                case AzurePipelinesImage.Vs2017Win2016:
+                case AzurePipelinesImage.Vs2015Win2012R2:
+                case AzurePipelinesImage.Win1803:
+                    targetToExcludes = new []{ nameof(TestCoreOnly), nameof(CompileCoreOnly), nameof(PackCoreOnly)};
+                    break;
+                case AzurePipelinesImage.Ubuntu1604:
+                case AzurePipelinesImage.Ubuntu1804:
+                case AzurePipelinesImage.UbuntuLatest:
+                case AzurePipelinesImage.MacOsLatest:
+                case AzurePipelinesImage.MacOs1014:
+                case AzurePipelinesImage.MacOs1013:
+                    targetToExcludes = new[] { nameof(Test), nameof(Compile), nameof(Pack) };
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(image), image, null);
+            }
+
+            var filterRelevantTargets = relevantTargets.Where(x => !targetToExcludes.Contains(x.Name)).ToList();
+
+            return base.GetStage(image, filterRelevantTargets);
+        }
+
+        protected override AzurePipelinesJob GetJob(ExecutableTarget executableTarget,
+            LookupTable<ExecutableTarget, AzurePipelinesJob> jobs)
+        {
+            var job = base.GetJob(executableTarget, jobs);
+
+            var dictionary = new Dictionary<string, string>
+            {
+                {nameof(Compile), "⚙️"},
+                {nameof(CompileCoreOnly), "⚙️"},
+                {nameof(Test), "🚦"},
+                {nameof(TestCoreOnly), "🚦"},
+                {nameof(Pack), "📦"},
+                {nameof(PackCoreOnly), "📦"},
+            };
+            var symbol = dictionary.GetValueOrDefault(job.Name);
+            var prefix = symbol == null ? "" : $"{symbol} ";
+            job.DisplayName = job.PartitionName == null
+                ? $"{prefix}{job.DisplayName}"
+                : $"{prefix}{job.DisplayName} 🧩";
+            return job;
+        }
+    }
+}
---
+++ Build/Nuke/Build.cs
@@ -0,0 +1,211 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.AppVeyor;
+using Nuke.Common.CI.AzurePipelines;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+[GitHubActions(
+    "continuous",
+    GitHubActionsImage.WindowsLatest,
+    On = new[] { GitHubActionsTrigger.Push },
+    ImportGitHubTokenAs = nameof(GitHubToken),
+    InvokedTargets = new[] { nameof(Test), nameof(Pack) })]
+[GitHubActions(
+    "continuousCore",
+    GitHubActionsImage.UbuntuLatest,
+    GitHubActionsImage.MacOsLatest,
+    On = new[] { GitHubActionsTrigger.Push },
+    ImportGitHubTokenAs = nameof(GitHubToken),
+    InvokedTargets = new[] { nameof(TestCoreOnly) })]
+[AppVeyor(
+    AppVeyorImage.VisualStudio2019,
+    SkipTags = true,
+    InvokedTargets = new[] { nameof(Test), nameof(Pack) })]
+[AzurePipelines(
+    suffix: null,
+    AzurePipelinesImage.WindowsLatest,
+    AzurePipelinesImage.UbuntuLatest,
+    AzurePipelinesImage.MacOsLatest,
+    InvokedTargets = new[] { nameof(Test), nameof(TestCoreOnly), nameof(Pack), nameof(PackCoreOnly) },
+    NonEntryTargets = new[] { nameof(Restore) },
+    ExcludedTargets = new[] { nameof(Clean)})]
+
+partial class Build : Nuke.Common.NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
+
+    [CI] readonly AzurePipelines AzurePipelines;
+    [Parameter("GitHub Token")] readonly string GitHubToken;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath ResultDirectory => RootDirectory / ".result";
+    AbsolutePath PackagesDirectory => ResultDirectory / "packages";
+    AbsolutePath TestResultDirectory => ResultDirectory / "test-results";
+    IEnumerable<Project> TestProjects => Solution.GetProjects("*.Tests");
+    IEnumerable<Project> AllProjects => Solution.AllProjects.Where(x=> SourceDirectory.Contains(x.Path));
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            EnsureCleanDirectory(ResultDirectory);
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() => ExecutesCompile(false));
+    void ExecutesCompile(bool excludeNetFramework)
+    {
+        Logger.Info(excludeNetFramework ? "Exclude net framework" : "Include net framework");
+        if (excludeNetFramework)
+        {
+            var frameworks =
+                from project in AllProjects
+                from framework in project.GetTargetFrameworks(true)
+                select new {project, framework};
+
+
+            DotNetBuild(s => s
+                .SetConfiguration(Configuration)
+                .SetNoRestore(InvokedTargets.Contains(Restore))
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .CombineWith(frameworks, (s, f) => s
+                    .SetFramework(f.framework)
+                    .SetProjectFile(f.project)));
+        }
+        else
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore()
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion));
+        }
+    }
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Produces(TestResultDirectory / "*.trx")
+        .Executes(() => ExecutesTest(false));
+    void ExecutesTest(bool excludeNetFramework)
+    {
+        Logger.Info(excludeNetFramework ? "Exclude net framework" : "Include net framework");
+
+        var testConfigurations =
+            from project in TestProjects
+            from framework in project.GetTargetFrameworks(excludeNetFramework)
+            select new {project, framework};
+
+        DotNetTest(_ =>
+            {
+                return _
+                    .SetConfiguration(Configuration)
+                    .SetNoRestore(InvokedTargets.Contains(Restore))
+                    .SetNoBuild(InvokedTargets.Contains(Compile))
+                    .ResetVerbosity()
+                    .SetResultsDirectory(TestResultDirectory)
+                    .CombineWith(testConfigurations, (_, v) => _
+                        .SetProjectFile(v.project)
+                        .SetFramework(v.framework)
+                        .SetLogger($"trx;LogFileName={v.project.Name}.trx"));
+            });
+
+        TestResultDirectory.GlobFiles("*.trx").ForEach(x =>
+            AzurePipelines?.PublishTestResults(
+                type: AzurePipelinesTestResultsType.VSTest,
+                title: $"{Path.GetFileNameWithoutExtension(x)} ({AzurePipelines.StageDisplayName})",
+                files: new string[] {x}));
+    }
+
+    Target CompileCoreOnly => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            var excludeNetFramework = AllProjects.SelectMany(x => x.GetTargetFrameworks()).Distinct()
+                .Any(x => !x.Contains("standard") || !x.Contains("core"));
+            ExecutesCompile(excludeNetFramework);
+        });
+
+    Target TestCoreOnly => _ => _
+        .DependsOn(CompileCoreOnly)
+        .Produces(TestResultDirectory / "*.trx")
+        .Executes(() =>
+        {
+            var excludeNetFramework = AllProjects.SelectMany(x => x.GetTargetFrameworks()).Distinct()
+                .Any(x => !x.Contains("standard") || !x.Contains("core"));
+            ExecutesTest(excludeNetFramework);
+        });
+
+
+    Target Pack => _ => _
+        .DependsOn(Compile)
+        .Produces(PackagesDirectory / "*.nupkg")
+        .Executes(ExecutesPack);
+
+    Target PackCoreOnly => _ => _
+        .DependsOn(CompileCoreOnly)
+        .Produces(PackagesDirectory / "*.nupkg")
+        .Executes(ExecutesPack);
+
+
+    void ExecutesPack() =>
+        DotNetPack(_ =>
+        {
+            var repositoryUrl = $"https://{GitRepository.Endpoint}/{GitRepository.Identifier}/";
+
+            return _    
+                .SetProject(Solution)
+                .SetNoRestore(InvokedTargets.Contains(Restore))
+                .SetNoBuild(InvokedTargets.Contains(Compile))
+                .SetConfiguration(Configuration)
+                .SetOutputDirectory(PackagesDirectory)
+                .DisablePackageRequireLicenseAcceptance()
+                .SetRepositoryType("git")
+                .SetRepositoryUrl(repositoryUrl)
+                .SetProperty("RepositoryCommit", GitVersion.Sha)
+                .SetPackageReleaseNotes($"{repositoryUrl}releases/v{GitVersion.MajorMinorPatch}")
+                .SetAuthors("Reegeek")
+                .SetProperty("Owners", "Reegeek")
+                .SetPackageProjectUrl(repositoryUrl)
+                .SetVersion(GitVersion.NuGetVersionV2);
+        });
+}
