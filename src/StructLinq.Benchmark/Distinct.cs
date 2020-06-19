033bea9e2055fc65104c63438c54a616a4588fbd
Reegeek <regis.fricker@gmail.com>
2020-06-19 10:15:06 +0200

    [Distinct] add benchmark result.

--- src/StructLinq.Benchmark/Distinct.cs
+++ src/StructLinq.Benchmark/Distinct.cs
@@ -5,6 +5,23 @@ using BenchmarkDotNet.Attributes;
 
 namespace StructLinq.Benchmark
 {
+    //``` ini
+
+    //BenchmarkDotNet=v0.12.0, OS=Windows 10.0.18363
+    //Intel Core i7-7700 CPU 3.60GHz (Kaby Lake), 1 CPU, 8 logical and 4 physical cores
+    //.NET Core SDK=3.1.301
+    //[Host]     : .NET Core 3.1.5 (CoreCLR 4.700.20.26901, CoreFX 4.700.20.27001), X64 RyuJIT
+    //DefaultJob : .NET Core 3.1.5 (CoreCLR 4.700.20.26901, CoreFX 4.700.20.27001), X64 RyuJIT
+
+
+    //```
+    //|                 Method |     Mean |    Error |   StdDev | Ratio | RatioSD |   Gen 0 |   Gen 1 |   Gen 2 | Allocated |
+    //|----------------------- |---------:|---------:|---------:|------:|--------:|--------:|--------:|--------:|----------:|
+    //|                   Linq | 558.6 us | 12.65 us | 18.94 us |  1.00 |    0.00 | 90.8203 | 90.8203 | 90.8203 |  524784 B |
+    //|             StructLinq | 311.2 us |  5.27 us |  4.67 us |  0.56 |    0.02 |       - |       - |       - |         - |
+    //|    ZeroAllocStructLinq | 240.0 us |  3.15 us |  2.95 us |  0.43 |    0.01 |       - |       - |       - |         - |
+    //| ZeroAllocStructLinqSum | 235.1 us |  1.97 us |  1.84 us |  0.43 |    0.01 |       - |       - |       - |         - |
+
     [MemoryDiagnoser]
     public class Distinct
     {
