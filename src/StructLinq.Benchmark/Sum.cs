da88630db89f84509983266485981ec2e6cb7501
Reegeek <regis.fricker@gmail.com>
2020-06-19 14:42:46 +0200

    [Sum] Add foreach on IStructEnumerable in benchmark (same result that Sum extensions).

--- src/StructLinq.Benchmark/Sum.cs
+++ src/StructLinq.Benchmark/Sum.cs
@@ -4,20 +4,23 @@ using BenchmarkDotNet.Attributes;
 namespace StructLinq.Benchmark
 {
 
+    //``` ini
+
     //BenchmarkDotNet=v0.12.0, OS=Windows 10.0.18363
     //Intel Core i7-7700 CPU 3.60GHz (Kaby Lake), 1 CPU, 8 logical and 4 physical cores
-    //.NET Core SDK=3.1.101
-    //[Host]     : .NET Core 3.1.1 (CoreCLR 4.700.19.60701, CoreFX 4.700.19.60801), X64 RyuJIT
-    //DefaultJob : .NET Core 3.1.1 (CoreCLR 4.700.19.60701, CoreFX 4.700.19.60801), X64 RyuJIT
+    //.NET Core SDK=3.1.301
+    //[Host]     : .NET Core 3.1.5 (CoreCLR 4.700.20.26901, CoreFX 4.700.20.27001), X64 RyuJIT
+    //DefaultJob : .NET Core 3.1.5 (CoreCLR 4.700.20.26901, CoreFX 4.700.20.27001), X64 RyuJIT
 
 
     //```
-    //|     Method |      Mean |     Error |    StdDev | Ratio | RatioSD | Gen 0 | Gen 1 | Gen 2 | Allocated |
-    //|----------- |----------:|----------:|----------:|------:|--------:|------:|------:|------:|----------:|
-    //|     ForSum |  3.004 us | 0.0598 us | 0.1000 us |  1.00 |    0.00 |     - |     - |     - |         - |
-    //|     SysSum | 40.905 us | 0.5443 us | 0.5092 us | 13.38 |    0.53 |     - |     - |     - |      40 B |
-    //|  StructSum |  2.969 us | 0.0390 us | 0.0365 us |  0.97 |    0.03 |     - |     - |     - |         - |
-    //| ConvertSum | 40.720 us | 0.5673 us | 0.5306 us | 13.32 |    0.58 |     - |     - |     - |      40 B |
+    //|           Method |      Mean |     Error |    StdDev | Ratio | RatioSD | Gen 0 | Gen 1 | Gen 2 | Allocated |
+    //|----------------- |----------:|----------:|----------:|------:|--------:|------:|------:|------:|----------:|
+    //|           ForSum |  3.072 us | 0.0540 us | 0.0506 us |  1.00 |    0.00 |     - |     - |     - |         - |
+    //|           SysSum | 41.873 us | 0.5678 us | 0.5311 us | 13.63 |    0.27 |     - |     - |     - |      40 B |
+    //|        StructSum |  3.087 us | 0.0317 us | 0.0296 us |  1.00 |    0.02 |     - |     - |     - |         - |
+    //| StructForEachSum |  3.090 us | 0.0300 us | 0.0281 us |  1.01 |    0.01 |     - |     - |     - |         - |
+    //|       ConvertSum | 48.592 us | 0.7864 us | 0.7356 us | 15.82 |    0.28 |     - |     - |     - |      40 B |
 
 
     [MemoryDiagnoser, DisassemblyDiagnoser(recursiveDepth: 4)]
@@ -50,6 +53,17 @@ namespace StructLinq.Benchmark
         public int StructSum()
         {
             return StructEnumerable.Range(0, Count).Sum(x=>x);
+        }
+
+        [Benchmark]
+        public int StructForEachSum()
+        {
+            var sum = 0;
+            foreach (var i in StructEnumerable.Range(0, Count))
+            {
+                sum += i;
+            }
+            return sum;
         }
 
         [Benchmark]
