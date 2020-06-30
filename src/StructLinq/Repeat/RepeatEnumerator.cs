0f330f0966fb7f0df997bbc97524ff71f3d1034d
Reegeek <regis.fricker@gmail.com>
2020-06-30 16:35:24 +0200

    [Repeat] add Repeat generator.

---
+++ src/StructLinq/Repeat/RepeatEnumerator.cs
@@ -0,0 +1,31 @@
+﻿namespace StructLinq.Repeat
+{
+    public struct RepeatEnumerator<T> : IStructEnumerator<T>
+    {
+        private readonly T element;
+        private readonly uint count;
+        private uint index;
+        public RepeatEnumerator(T element, uint count)
+        {
+            this.element = element;
+            this.count = count;
+            index = 0;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            return index++ < count;
+        }
+
+        public void Reset()
+        {
+            index = 0;
+        }
+
+        public T Current => element;
+    }
+}
