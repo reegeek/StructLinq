9b11eefa4c9916da15aa7103120032fefd434192
Reegeek <10356780+reegeek@users.noreply.github.com>
2020-07-07 17:13:15 +0200

    [Nuke] try to fix build.

--- Build/Nuke/ProjectExtensions.cs
+++ Build/Nuke/ProjectExtensions.cs
@@ -8,7 +8,8 @@ public static class ProjectExtensions
     {
         var frameworks = project
             .GetTargetFrameworks()
-            .Where(x=> !x.Contains("netcoreapp1"));
+            //because github actions and azure pipeline does not handle it.
+            .Where(x=> !x.Contains("netcoreapp1") && x.Contains("netcoreapp2.2"));
         if (!excludeNetFramework)
             return frameworks.ToList();
         return frameworks.Where(x => x.Contains("standard") || x.Contains("core")).ToList();
