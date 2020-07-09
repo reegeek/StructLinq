592a8e6866f066c1f95c696b031f7341ddb59be3
Reegeek <10356780+reegeek@users.noreply.github.com>
2020-07-09 17:11:59 +0200

    [Collection] add PooledList

---
+++ src/StructLinq.Tests/Utils/Collections/PooledListTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Buffers;
+using System.Linq;
+using StructLinq.Utils.Collections;
+using Xunit;
+
+namespace StructLinq.Tests.Utils.Collections
+{
+    public class PooledListTests
+    {
+        [Fact]
+        public void ShouldIncreaseCapacity()
+        {
+            var list = new PooledList<int>(0, ArrayPool<int>.Shared);
+            var count = 100;
+            var array = Enumerable.Range(0, count).ToArray();
+            foreach (var i in array)
+            {
+                list.Add(i);
+            }
+
+            Assert.Equal(count, list.Size);
+            for (int i = 0; i < count; i++)
+            {
+                Assert.Equal(list.Items[i], array[i]);
+            }
+        }
+
+        [Fact]
+        public void ShouldIncreaseCapacityWithRef()
+        {
+            var list = new PooledList<int>(0, ArrayPool<int>.Shared);
+            var count = 100;
+            var array = Enumerable.Range(0, count).ToArray();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(ref array[i]);
+            }
+
+            Assert.Equal(count, list.Size);
+            for (int i = 0; i < count; i++)
+            {
+                Assert.Equal(list.Items[i], array[i]);
+            }
+        }
+
+    }
+}
---
+++ src/StructLinq/Utils/Collections/PooledList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+
+namespace StructLinq.Utils.Collections
+{
+    internal struct PooledList<T> : IDisposable
+    {
+        private static readonly T[] emptyArray = new T[0];
+
+        private readonly ArrayPool<T> pool;
+        private int index;
+
+        public PooledList(int capacity, ArrayPool<T> pool)
+        {
+            this.pool = pool;
+            Items = capacity > 0 ? pool.Rent(capacity) : emptyArray;
+            index = -1;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void IncreaseCapacity()
+        {
+            var newItems = pool.Rent(index + 1);
+            if (index > 0)
+                System.Array.Copy(Items, newItems, index);
+            ReturnArray();
+            Items = newItems;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void ReturnArray()
+        {
+            if (Items.Length == 0)
+                return;
+
+            try
+            {
+                // Clear the elements so that the gc can reclaim the references.
+                pool.Return(Items);
+            }
+            catch (ArgumentException)
+            {
+                // oh well, the array pool didn't like our array
+            }
+
+            Items = emptyArray;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(T item)
+        {
+            if (++index >= Items.Length)
+                IncreaseCapacity();
+            Items[index] = item;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(ref T item)
+        {
+            if (++index >= Items.Length)
+                IncreaseCapacity();
+            ref var elt = ref Items[index];
+            elt = item;
+        }
+
+
+        internal T[] Items;
+
+        internal int Size
+        {
+            get
+            {
+                return index + 1;
+
+            }
+        }
+
+        public void Dispose()
+        {
+            ReturnArray();
+            index = -1;
+        }
+    }
+}
