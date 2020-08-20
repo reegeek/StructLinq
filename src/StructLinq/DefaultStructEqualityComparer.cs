82f8989a51bb6cd6c1aa3525aeab2e0d5371e296
Reegeek <10356780+reegeek@users.noreply.github.com>
2020-08-20 15:37:51 +0200

    [Comparer] create default comparer for some ValueType.

---
+++ src/StructLinq/DefaultStructEqualityComparer.cs
@@ -0,0 +1,100 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+
+namespace StructLinq
+{
+    public readonly struct DefaultStructEqualityComparer :
+        IEqualityComparer<Int16> 
+            ,
+        IEqualityComparer<Int32> 
+            ,
+        IEqualityComparer<Int64> 
+            ,
+        IEqualityComparer<UInt16> 
+            ,
+        IEqualityComparer<UInt32> 
+            ,
+        IEqualityComparer<UInt64> 
+            ,
+        IEqualityComparer<Single> 
+            ,
+        IEqualityComparer<Double> 
+            ,
+        IEqualityComparer<Byte> 
+            ,
+        IEqualityComparer<SByte> 
+            ,
+        IEqualityComparer<DateTime> 
+        {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(Int16 x, Int16 y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(Int16 value) => value.GetHashCode();
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(Int32 x, Int32 y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(Int32 value) => value.GetHashCode();
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(Int64 x, Int64 y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(Int64 value) => value.GetHashCode();
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(UInt16 x, UInt16 y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(UInt16 value) => value.GetHashCode();
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(UInt32 x, UInt32 y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(UInt32 value) => value.GetHashCode();
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(UInt64 x, UInt64 y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(UInt64 value) => value.GetHashCode();
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(Single x, Single y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(Single value) => value.GetHashCode();
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(Double x, Double y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(Double value) => value.GetHashCode();
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(Byte x, Byte y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(Byte value) => value.GetHashCode();
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(SByte x, SByte y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(SByte value) => value.GetHashCode();
+
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(DateTime x, DateTime y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(DateTime value) => value.GetHashCode();
+
+    
+    }
+}
---
+++ src/StructLinq/DefaultStructInEqualityComparer.cs
@@ -0,0 +1,99 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace StructLinq
+{
+    public readonly struct DefaultStructInEqualityComparer :
+        IInEqualityComparer<Int16> 
+            ,
+        IInEqualityComparer<Int32> 
+            ,
+        IInEqualityComparer<Int64> 
+            ,
+        IInEqualityComparer<UInt16> 
+            ,
+        IInEqualityComparer<UInt32> 
+            ,
+        IInEqualityComparer<UInt64> 
+            ,
+        IInEqualityComparer<Single> 
+            ,
+        IInEqualityComparer<Double> 
+            ,
+        IInEqualityComparer<Byte> 
+            ,
+        IInEqualityComparer<SByte> 
+            ,
+        IInEqualityComparer<DateTime> 
+        {
+                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(in Int16 x,in Int16 y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(in Int16 value) => value.GetHashCode();
+
+                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(in Int32 x,in Int32 y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(in Int32 value) => value.GetHashCode();
+
+                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(in Int64 x,in Int64 y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(in Int64 value) => value.GetHashCode();
+
+                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(in UInt16 x,in UInt16 y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(in UInt16 value) => value.GetHashCode();
+
+                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(in UInt32 x,in UInt32 y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(in UInt32 value) => value.GetHashCode();
+
+                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(in UInt64 x,in UInt64 y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(in UInt64 value) => value.GetHashCode();
+
+                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(in Single x,in Single y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(in Single value) => value.GetHashCode();
+
+                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(in Double x,in Double y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(in Double value) => value.GetHashCode();
+
+                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(in Byte x,in Byte y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(in Byte value) => value.GetHashCode();
+
+                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(in SByte x,in SByte y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(in SByte value) => value.GetHashCode();
+
+                 [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool Equals(in DateTime x,in DateTime y) => x == y;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int GetHashCode(in DateTime value) => value.GetHashCode();
+
+    
+    }
+}
