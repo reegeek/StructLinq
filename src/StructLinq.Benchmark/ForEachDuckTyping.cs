0086f2257c21b73775fc7128fdb9f6605f3fb08b
Reegeek <10356780+reegeek@users.noreply.github.com>
2020-09-24 12:21:45 +0200

    [ForEach] Add for loop in bench.

--- src/StructLinq.Benchmark/ForEachDuckTyping.cs
+++ src/StructLinq.Benchmark/ForEachDuckTyping.cs
@@ -5,42 +5,76 @@ using BenchmarkDotNet.Attributes;
 namespace StructLinq.Benchmark
 {
 
-    //BenchmarkDotNet=v0.12.0, OS=Windows 10.0.18363
-    //Intel Core i7-8750H CPU 2.20GHz(Coffee Lake), 1 CPU, 12 logical and 6 physical cores
-    //.NET Core SDK = 3.1.201
+    //``` ini
 
-    //[Host]     : .NET Core 3.1.3 (CoreCLR 4.700.20.11803, CoreFX 4.700.20.12001), X64 RyuJIT
-    //DefaultJob : .NET Core 3.1.3 (CoreCLR 4.700.20.11803, CoreFX 4.700.20.12001), X64 RyuJIT
+    //BenchmarkDotNet=v0.12.0, OS=Windows 10.0.19041
+    //Intel Core i7-7700 CPU 3.60GHz (Kaby Lake), 1 CPU, 8 logical and 4 physical cores
+    //.NET Core SDK=3.1.402
+    //[Host]     : .NET Core 3.1.8 (CoreCLR 4.700.20.41105, CoreFX 4.700.20.41903), X64 RyuJIT
+    //DefaultJob : .NET Core 3.1.8 (CoreCLR 4.700.20.41105, CoreFX 4.700.20.41903), X64 RyuJIT
 
 
     //```
     //|                                      Method |      Mean |     Error |    StdDev | Ratio | RatioSD | Gen 0 | Gen 1 | Gen 2 | Allocated |
     //|-------------------------------------------- |----------:|----------:|----------:|------:|--------:|------:|------:|------:|----------:|
-    //|                              ForEachOnArray |  5.217 us | 0.0336 us | 0.0314 us |  1.00 |    0.00 |     - |     - |     - |         - |
-    //|                        ForEachOnIEnumerable | 39.114 us | 0.2016 us | 0.1886 us |  7.50 |    0.06 |     - |     - |     - |      32 B |
-    //|              ForEachOnArrayStructEnumerable |  5.195 us | 0.0189 us | 0.0176 us |  1.00 |    0.01 |     - |     - |     - |         - |
-    //| ForEachOnArrayStructEnumerableAsIEnumerable | 68.004 us | 0.1789 us | 0.1674 us | 13.04 |    0.09 |     - |     - |     - |      32 B |
+    //|                                  ForOnArray |  6.264 us | 0.1056 us | 0.0988 us |  1.00 |    0.00 |     - |     - |     - |         - |
+    //|                              ForEachOnArray |  4.627 us | 0.0907 us | 0.1516 us |  0.74 |    0.02 |     - |     - |     - |         - |
+    //|                    ForEachOnYieldEnumerable | 51.903 us | 0.4945 us | 0.4626 us |  8.29 |    0.15 |     - |     - |     - |      57 B |
+    //|                        ForEachOnIEnumerable | 46.738 us | 0.6210 us | 0.5505 us |  7.46 |    0.15 |     - |     - |     - |      32 B |
+    //|              ForEachOnArrayStructEnumerable |  5.782 us | 0.0675 us | 0.0631 us |  0.92 |    0.02 |     - |     - |     - |         - |
+    //| ForEachOnArrayStructEnumerableAsIEnumerable | 57.834 us | 0.4597 us | 0.4075 us |  9.24 |    0.18 |     - |     - |     - |      64 B |
 
-    [MemoryDiagnoser]
+    [DisassemblyDiagnoser(recursiveDepth: 4), MemoryDiagnoser]
     public class ForEachDuckTyping
     {
         private const int Count = 10_000;
         private readonly int[] array;
         private readonly IEnumerable<int> sysEnumerable;
-        private readonly IEnumerable<int> arrayEnumerableAsIEnumerable;
 
         public ForEachDuckTyping()
         {
             array = Enumerable.Range(0, Count).ToArray();
             sysEnumerable = Enumerable.Range(0, Count).ToArray();
-            arrayEnumerableAsIEnumerable = Enumerable.Range(0, Count).ToArray().ToStructEnumerable().ToEnumerable();
+        }
+
+        private IEnumerable<int> YieldEnumerable()
+        {
+            var arrayLength = array.Length;
+            var tab = array;
+            for (int i = 0; i < arrayLength; i++)
+            {
+                yield return tab[i];
+            }
         }
 
         [Benchmark(Baseline = true)]
+        public int ForOnArray()
+        {
+            var sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+
+
+        [Benchmark]
         public int ForEachOnArray()
         {
             var sum = 0;
             foreach (var i in array)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        [Benchmark]
+        public int ForEachOnYieldEnumerable()
+        {
+            var sum = 0;
+            foreach (var i in YieldEnumerable())
             {
                 sum += i;
             }
@@ -73,7 +107,7 @@ namespace StructLinq.Benchmark
         public int ForEachOnArrayStructEnumerableAsIEnumerable()
         {
             var sum = 0;
-            foreach (var i in arrayEnumerableAsIEnumerable)
+            foreach (var i in array.ToStructEnumerable().ToEnumerable())
             {
                 sum += i;
             }
