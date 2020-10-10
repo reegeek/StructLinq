8efe2edf0bbb8262b416ab77f8553d46bc040ce2
Reegeek <10356780+reegeek@users.noreply.github.com>
2020-10-10 21:21:30 +0200

    [Tests] fix compilation.

--- src/StructLinq.Tests/OrderByTests.cs
+++ src/StructLinq.Tests/OrderByTests.cs
@@ -37,36 +37,4 @@ namespace StructLinq.Tests
             Assert.Equal(expected, order);
         }
     }
-
-    public class OrderByDescendingTests : AbstractEnumerableTests<
-        int,
-        OrderByKeyEnumerable<int, IStructEnumerable<int, ArrayStructEnumerator<int>>, ArrayStructEnumerator<int>, StructFunction<int, int>, int, Comparer<int>>,
-        OrderByEnumerator<int>>
-    {
-        protected override OrderByKeyEnumerable<int, IStructEnumerable<int, ArrayStructEnumerator<int>>, ArrayStructEnumerator<int>, StructFunction<int, int>, int, Comparer<int>> Build(int size)
-        {
-            var array = Enumerable.Range(-1, size).Reverse().ToArray().ToStructEnumerable();
-            Func<int, int> func = x => x;
-            var orderByKeyEnumerable = array.OrderByDescending(func);
-            return orderByKeyEnumerable;
-        }
-
-        [Fact]
-        public void ShouldBeSameAsSystem()
-        {
-            var rand = new Random(42);
-            var list = new List<int>();
-            for (int i = 0; i < 100; i++)
-            {
-                list.Add(rand.Next());
-            }
-            var array = list.ToArray();
-
-            var order = array.ToStructEnumerable().OrderByDescending(x => x).ToArray();
-            var expected = array.OrderByDescending(x => x).ToArray();
-
-            Assert.Equal(expected, order);
-        }
-    }
-
 }
