c1970683a7f6966d02240d9661c53ba4ccc2a2ed
Reegeek <10356780+reegeek@users.noreply.github.com>
2020-11-03 18:01:15 +0100

    Add PooledListVisitor

---
+++ src/StructLinq.Tests/Utils/Collections/PooledListVisitorTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Buffers;
+using System.Linq;
+using StructLinq.Utils.Collections;
+using Xunit;
+
+namespace StructLinq.Tests.Utils.Collections
+{
+    public class PooledListVisitorTests
+    {
+        [Fact]
+        public void ShouldCopyArray()
+        {
+            var visitor = new PooledListVisitor<int>(0, ArrayPool<int>.Shared);
+            var array = Enumerable.Range(-1, 50).ToArray();
+
+            array.ToStructEnumerable().Visit(ref visitor);
+
+            var value = visitor.PooledList.ToArray();
+            Assert.Equal(array, value);
+        }
+    }
+}
---
+++ src/StructLinq/Utils/Collections/PooledListVisitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+
+namespace StructLinq.Utils.Collections
+{
+    internal struct PooledListVisitor<T> : IVisitor<T>, IDisposable
+    {
+        public PooledList<T> PooledList;
+
+        public PooledListVisitor(int capacity, ArrayPool<T> pool)
+        {
+            PooledList = new PooledList<T>(capacity, pool);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Visit(T input)
+        {
+            PooledList.Add(input);
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose()
+        {
+            PooledList.Dispose();
+        }
+    }
+}
