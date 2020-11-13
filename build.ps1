4978faf56dc04c17be37ba2f4fa23938d5e966a6
Reegeek <10356780+reegeek@users.noreply.github.com>
2020-11-13 10:41:24 +0100

    [Build] force dotnet sdk download.

--- build.ps1
+++ build.ps1
@@ -14,14 +14,18 @@ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 ###########################################################################
 
 $BuildProjectFile = "$PSScriptRoot\Build\Nuke\NukeBuild.csproj"
-$TempDirectory = "$PSScriptRoot\\.tmp"
 
-$DotNetGlobalFile = "PSScriptRoot\Build\Nuke\global.json"
+$DotNetGlobalFile = "$PSScriptRoot\Build\Nuke\global.json"
+
+$TempDirectory = "$PSScriptRoot\.tmp"
+
 $DotNetInstallUrl = "https://dot.net/v1/dotnet-install.ps1"
 $DotNetChannel = "Current"
 
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
+$env:DOTNET_MULTILEVEL_LOOKUP = 0
+$env:DOTNET_ROLL_FORWARD = "Major"
 
 ###########################################################################
 # EXECUTION
@@ -32,42 +36,44 @@ function ExecSafe([scriptblock] $cmd) {
     if ($LASTEXITCODE) { exit $LASTEXITCODE }
 }
 
+# # Print environment variables
+# Get-Item -Path Env:* | Sort-Object -Property Name | ForEach-Object {"{0}={1}" -f $_.Name,$_.Value}
 
-# Print environment variables
-Get-Item -Path Env:* | Sort-Object -Property Name | ForEach-Object {"{0}={1}" -f $_.Name,$_.Value}
-
-# If global.json exists, load expected version
-if (Test-Path $DotNetGlobalFile) {
-    $DotNetGlobal = $(Get-Content $DotNetGlobalFile | Out-String | ConvertFrom-Json)
-    if ($DotNetGlobal.PSObject.Properties["sdk"] -and $DotNetGlobal.sdk.PSObject.Properties["version"]) {
-        $DotNetVersion = $DotNetGlobal.sdk.version
+    # If global.json exists, load expected version
+    if (Test-Path $DotNetGlobalFile) {
+        $DotNetGlobal = $(Get-Content $DotNetGlobalFile | Out-String | ConvertFrom-Json)
+        if ($DotNetGlobal.PSObject.Properties["sdk"] -and $DotNetGlobal.sdk.PSObject.Properties["version"]) {
+            $DotNetVersion = $DotNetGlobal.sdk.version
+			Write-Output "Request DotNetVersion $($DotNetVersion)..."
+        }
     }
-}
 
-# If dotnet is installed locally, and expected version is not set or installation matches the expected version
-if ($null -ne (Get-Command "dotnet" -ErrorAction SilentlyContinue) -and `
-     (!(Test-Path variable:DotNetVersion) -or $(& dotnet --version) -eq $DotNetVersion)) {
-    $env:DOTNET_EXE = (Get-Command "dotnet").Path
-}
-else {
-    $DotNetDirectory = "$TempDirectory\dotnet-win"
-    $env:DOTNET_EXE = "$DotNetDirectory\dotnet.exe"
-
+# If dotnet CLI is installed globally and it matches requested version, use for execution
+# if ($null -ne (Get-Command "dotnet" -ErrorAction SilentlyContinue) -and `
+    # $(dotnet --version) -and $LASTEXITCODE -eq 0) {
+    # $env:DOTNET_EXE = (Get-Command "dotnet").Path
+# }
+# else {
     # Download install script
     $DotNetInstallFile = "$TempDirectory\dotnet-install.ps1"
     New-Item -ItemType Directory -Path $TempDirectory -Force | Out-Null
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     (New-Object System.Net.WebClient).DownloadFile($DotNetInstallUrl, $DotNetInstallFile)
 
+
     # Install by channel or version
+    $DotNetDirectory = "$TempDirectory\dotnet-win"
     if (!(Test-Path variable:DotNetVersion)) {
         ExecSafe { & $DotNetInstallFile -InstallDir $DotNetDirectory -Channel $DotNetChannel -NoPath }
     } else {
         ExecSafe { & $DotNetInstallFile -InstallDir $DotNetDirectory -Version $DotNetVersion -NoPath }
     }
-}
+    $env:DOTNET_EXE = "$DotNetDirectory\dotnet.exe"
+# }
+
+Write-Output "dotnet path $($env:DOTNET_EXE)"
 
 Write-Output "Microsoft (R) .NET Core SDK version $(& $env:DOTNET_EXE --version)"
 
-ExecSafe { & $env:DOTNET_EXE build $BuildProjectFile /nodeReuse:false -nologo -clp:NoSummary --verbosity quiet }
+ExecSafe { & $env:DOTNET_EXE build $BuildProjectFile /nodeReuse:false /p:UseSharedCompilation=false -nologo -clp:NoSummary }
 ExecSafe { & $env:DOTNET_EXE run --project $BuildProjectFile --no-build -- $BuildArguments }
