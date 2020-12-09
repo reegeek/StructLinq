53989886346ae181fe86aa6f2a530012014be464
Reegeek <10356780+reegeek@users.noreply.github.com>
2020-12-09 09:44:59 +0100

    [Hashset] add hashset wrapper.

---
+++ src/StructLinq.BCL/Hashset/HashsetLayout.cs
@@ -0,0 +1,18 @@
+﻿namespace StructLinq.BCL.Hashset
+{
+    internal class HashsetLayout<T>
+    {
+        internal int[] Buckets;
+        internal Slot<T>[] Entries;
+        internal object Object1;
+        internal object Object2;
+    }
+
+    internal struct Slot<T>
+    {
+        internal int HashCode;
+        internal int Next;
+        internal T Value;
+    }
+
+}
---
+++ src/StructLinq.Tests/HashsetTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using StructLinq.BCL.Hashset;
+using Xunit;
+
+namespace StructLinq.Tests
+{
+    public class HashsetTests : AbstractCollectionTests<int,
+        HashsetEnumerable<int>,
+        HashsetEnumerator<int>>
+    {
+        protected override HashsetEnumerable<int> Build(int size)
+        {
+            return Enumerable
+                   .Range(-1, size)
+                   .ToHashSet()
+                   .ToStructEnumerable();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(10)]
+        [InlineData(50)]
+        public void ShouldSameAsSystem(int size)
+        {
+            var dictionary = Enumerable
+                             .Range(0, size)
+                             .ToHashSet();
+            var sysArray = dictionary
+                           .AsEnumerable()
+                           .ToArray();
+            var structArray = Enumerable
+                              .Range(0, size)
+                              .ToHashSet()
+                              .ToStructEnumerable()
+                              .ToEnumerable()
+                              .ToArray();
+            Assert.Equal(sysArray, structArray);
+        }
+
+        [Fact]
+        public void ShouldHandleAddElementAfterEnumerableWasCreated()
+        {
+            var dictionary = Enumerable.Range(0, 50).ToHashSet();
+            var structList = dictionary.ToStructEnumerable();
+            dictionary.Add(50);
+
+            var expected = Enumerable.Range(0, 51)
+                                     .ToHashSet()
+                                     .AsEnumerable()
+                                     .ToArray();
+            var enumerable = structList.ToEnumerable().ToArray();
+            Assert.Equal(expected, enumerable);
+        }
+
+    }
+}
