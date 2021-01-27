454278e6079348cc24d1469d2de9f33ecb087cd5
Reegeek <10356780+reegeek@users.noreply.github.com>
2021-01-27 15:00:48 +0100

    [Select] housekeeping.

--- src/StructLinq.Tests/RefSelectEnumerableTests.cs
+++ src/StructLinq.Tests/RefSelectEnumerableTests.cs
@@ -4,13 +4,13 @@ using StructLinq.Select;
 namespace StructLinq.Tests
 {
     public class RefSelectEnumerableTests : AbstractEnumerableTests<double,
-        RefSelectEnumerable<int, double, ArrayRefEnumerable<int>, ArrayRefStructEnumerator<int>, RefSelectCollectionTests.MultFunction>,
-        RefSelectEnumerator<int, double, ArrayRefStructEnumerator<int>, RefSelectCollectionTests.MultFunction>>
+        RefSelectEnumerable<int, double, ArrayRefEnumerable<int>, ArrayRefStructEnumerator<int>, RefSelectEnumerableTests.MultFunction>,
+        RefSelectEnumerator<int, double, ArrayRefStructEnumerator<int>, RefSelectEnumerableTests.MultFunction>>
     {
 
-        protected override RefSelectEnumerable<int, double, ArrayRefEnumerable<int>, ArrayRefStructEnumerator<int>, RefSelectCollectionTests.MultFunction> Build(int size)
+        protected override RefSelectEnumerable<int, double, ArrayRefEnumerable<int>, ArrayRefStructEnumerator<int>, RefSelectEnumerableTests.MultFunction> Build(int size)
         {
-            var func = new RefSelectCollectionTests.MultFunction();
+            var func = new RefSelectEnumerableTests.MultFunction();
             var selectEnumerable = 
                 StructEnumerable.Range(-1, size).ToArray().ToRefStructEnumerable().Select(ref func, x=>(IRefStructEnumerable<int, ArrayRefStructEnumerator<int>>)x, x => x);
             return selectEnumerable;
