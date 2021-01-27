a469febdb28bc30945ce204066f9d1d02135c7d0
Reegeek <10356780+reegeek@users.noreply.github.com>
2021-01-27 17:45:10 +0100

    [IEnumerable] add tests.

---
+++ src/StructLinq.Tests/StructEnumerableFromIEnumerableTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using StructLinq.IEnumerable;
+
+namespace StructLinq.Tests
+{
+    public class StructEnumerableFromIEnumerableTests: AbstractEnumerableTests<int,
+        StructEnumerableFromIEnumerable<int>,
+        GenericEnumerator<int>>
+    {
+        protected override StructEnumerableFromIEnumerable<int> Build(int size)
+        {
+            IEnumerable<int> enumerable = Enumerable.Range(0, size).ToArray();
+            return enumerable.ToStructEnumerable();
+        }
+    }
+}
