51d642101c8c8913a34c4f133cf462c22d1dada6
reegeek <10356780+reegeek@users.noreply.github.com>
2022-11-14 23:22:59 +0100

    [Infra] Split bcl test project to handle references. (#169)

---
+++ src/StructLinq.BCL.Tests/BitnessTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace StructLinq.BCL.Tests
+{
+#if IS_X86
+    public class BitnessTests
+    {
+        [Fact]
+        public void Checkx86Bitness()
+        {
+            Assert.Equal(4, IntPtr.Size);
+        }
+    }
+#endif
+
+#if IS_X64
+    public class BitnessTests
+    {
+        [Fact]
+        public void Checkx64Bitness()
+        {
+            Assert.Equal(8, IntPtr.Size);
+        }
+    }
+#endif
+}
