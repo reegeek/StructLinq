9fc4a6479bbef6217a88f1703d04144d68ed2cf0
Reegeek <10356780+reegeek@users.noreply.github.com>
2022-12-09 20:21:49 +0100

    [ToArray] add extension for RefStructEnum

---
+++ src/StructLinq/ToArray/RefStructEnumerable.ToArray.cs
@@ -0,0 +1,26 @@
+﻿// ReSharper disable once CheckNamespace
+
+using System.Buffers;
+using System.Runtime.CompilerServices;
+using StructLinq.Utils.Collections;
+
+// ReSharper disable once CheckNamespace
+namespace StructLinq
+{
+    public partial struct RefStructEnum<T, TEnumerator>
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T[] ToArray(int capacity, ArrayPool<T> pool)
+        {
+            var list = new PooledList<T>(capacity, pool);
+            var copy = enumerator;
+            PoolLists.FillRef(ref list, ref copy);
+            var array = list.ToArray();
+            list.Dispose();
+            return array;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T[] ToArray(int capacity = 0) => ToArray(capacity, ArrayPool<T>.Shared);
+    }
+}
